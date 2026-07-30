e070618d5c3f1e5a201da9ba4d744ec1b7440a42
GANESHPRABHU <gp561910@gmail.com>
2026-07-30 23:06:43 +0530

    Add ArraySorting class to sort and print an array

---
+++ ArraySorting.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class ArraySorting {
+    public static void main(String[] args) {
+
+        int[] arr = {50, 10, 40, 30, 20};
+
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
