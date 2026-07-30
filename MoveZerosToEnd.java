aaa6bda50a338bd898d02a2daaa6688c275e1c3f
GANESHPRABHU <gp561910@gmail.com>
2026-07-30 23:08:41 +0530

    Add functionality to move zeros to the end of array
    Implement logic to move all zeros in the array to the end while maintaining the order of non-zero elements.

---
+++ MoveZerosToEnd.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class MoveZerosToEnd {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 0, 2, 0, 3, 0, 4};
+
+        int index = 0;
+
+        for (int num : arr) {
+            if (num != 0) {
+                arr[index++] = num;
+            }
+        }
+
+        while (index < arr.length) {
+            arr[index++] = 0;
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
