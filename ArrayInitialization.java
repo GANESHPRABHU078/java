96247594fb51046952a96b7bf5895bef2764e5a3
GANESHPRABHU <gp561910@gmail.com>
2026-07-30 22:54:40 +0530

    Add array initialization and printing in main method
    Initialize an array with values and print them.

---
+++ ArrayInitialization.java
@@ -0,0 +1,10 @@
+public class ArrayInitialization {
+    public static void main(String[] args) {
+
+        int[] arr = {10, 20, 30, 40, 50};
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
