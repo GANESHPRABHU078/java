afafaad4c8d5e42c6bcd78eaaebde66a800fcf75
GANESHPRABHU <gp561910@gmail.com>
2026-07-30 22:58:02 +0530

    Implement ArrayMinimum class to find minimum value

---
+++ ArrayMinimum.java
@@ -0,0 +1,16 @@
+public class ArrayMinimum {
+    public static void main(String[] args) {
+
+        int[] arr = {15, 70, 22, 99, 8};
+
+        int min = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+
+        System.out.println("Minimum = " + min);
+    }
+}
