2e55e98401fbed485205d2e62e0cbb929236d307
GANESHPRABHU <gp561910@gmail.com>
2026-08-04 11:54:52 +0530

    Time: 1959 ms (5%), Space: 281 MB (5.52%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.java
+++ 0060-permutation-sequence/0060-permutation-sequence.java
@@ -1,28 +1,25 @@
 class Solution {
-    List<List<Integer>> list =new ArrayList<>();
+    List<StringBuilder> list =new ArrayList<>();
     public String getPermutation(int n, int k) {
         int nums[]=new int[n];
         for(int i=0;i<n;i++){
             nums[i]=i+1;
         }
         boolean seen []=new boolean[nums.length];
-        backtracking(nums,new ArrayList<>(),seen);
-        System.out.print(list.get(k-1));
-        String s ="";
-        for(int i:list.get(k-1)) s=s+(i+"");
-        return s;
+        backtracking(nums,new StringBuilder(),seen);
+        return list.get(k-1).toString();
     }
-     public  void backtracking(int []nums,List<Integer> curr,boolean[] seen){
-        if(curr.size()==nums.length){
-            list.add(new ArrayList<>(curr));
+     public  void backtracking(int []nums,StringBuilder curr,boolean[] seen){
+        if(curr.length()==nums.length){
+            list.add(new StringBuilder(curr));
             return;
         }
         for(int i=0;i<nums.length;i++){
             if(seen[i]) continue;
-            curr.add(nums[i]);
+            curr.append(nums[i]);
                seen[i]=true;
                backtracking(nums,curr,seen);
-               curr.remove(curr.size()-1);
+               curr.deleteCharAt(curr.length()-1);
                 seen[i]=false;
         }
     }
