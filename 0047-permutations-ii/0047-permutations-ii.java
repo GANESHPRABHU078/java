c7c129b48b71a2521b1e937b0da493f6daa7ada8
GANESHPRABHU <gp561910@gmail.com>
2026-08-04 13:30:51 +0530

    Time: 704 ms (5.12%), Space: 46.5 MB (89.09%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -8,11 +8,12 @@ class Solution {
     }
     void backtracking(int start,int []nums,List<Integer> list1,boolean []arr){
         if(list1.size()==nums.length){
+            if(!list.contains(list1))
             list.add(new ArrayList<>(list1));
         }
         for(int i=0;i<nums.length;i++){
             if(arr[i]) continue;
-             if(i>0&&nums[i]==nums[i-1]&&!arr[i-1]) continue;
+//if(i>0&&nums[i]==nums[i-1]&&!arr[i-1]) continue;
             list1.add(nums[i]);
             arr[i]=true;
             backtracking(start+1,nums,list1,arr);
