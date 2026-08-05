90ce73c5ed1eb6eb68ee50b924a269bbcdc9a5e6
GANESHPRABHU <gp561910@gmail.com>
2026-08-05 14:13:16 +0530

    Time: 404 ms (18.45%), Space: 276.5 MB (5.08%) - LeetHub

--- 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
+++ 2597-the-number-of-beautiful-subsets/2597-the-number-of-beautiful-subsets.java
@@ -1,27 +1,31 @@
 class Solution {
     List<List<Integer>> list = new ArrayList<>();
-    int []nums;
+    int[] nums;
     int k;
+
     public int beautifulSubsets(int[] nums, int k) {
-        this.nums=nums;
-        this.k=k;
-        subsets(0,new ArrayList<>());
-        return list.size()-1;
+        this.nums = nums;
+        this.k = k;
+        subsets(0, new ArrayList<>());
+        return list.size() - 1;
     }
-  void subsets(int start,List<Integer> list1){
-         if(ans(list1)) list.add(new ArrayList<>(list1));
-        for(int i=start;i<nums.length;i++){
-            list1.add(nums[i]);
-            subsets(i+1,list1);
-            list1.remove(list1.size()-1);
+
+    void subsets(int start, List<Integer> list1) {
+        // if (ans(list1,nums[i]))
+        list.add(new ArrayList<>(list1));
+        for (int i = start; i < nums.length; i++) {
+            if (ans(list1,nums[i])) {
+                list1.add(nums[i]);
+                subsets(i + 1, list1);
+                list1.remove(list1.size() - 1);
+            }
         }
     }
-    boolean ans(List<Integer> list1){
-        for(int i=0;i<list1.size();i++){
-            for(int j=i+1;j<list1.size();j++){
-                if((Math.abs((list1.get(j)-list1.get(i))))==k){
-                    return false;
-                }
+
+    boolean ans(List<Integer> list1, int num) {
+        for (int x : list1) {
+            if (Math.abs(x - num) == k) {
+                return false;
             }
         }
         return true;
